34f007d01b27efd63311b7d62105890534ca4052
Milan Marinkovic <milan_marinkovic98@hotmail.com>
2022-06-07 17:52:00 +0200

    test: :white_check_mark: add unit tests

---
+++ src/repository/MessageRepositoryMock.go
@@ -0,0 +1,52 @@
+package repository
+
+import (
+	"errors"
+	"messages-ms/src/entity"
+	"time"
+
+	"github.com/stretchr/testify/mock"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type MessageRepositoryMock struct {
+	mock.Mock
+}
+
+func (m MessageRepositoryMock) Create(message entity.Message) (entity.Message, error) {
+	if message.From == 5 {
+		return entity.Message{}, errors.New("")
+	}
+
+	message.ID = primitive.NewObjectID()
+
+	return message, nil
+}
+
+func (m MessageRepositoryMock) Update(message entity.Message) error {
+	if message.ID == primitive.NilObjectID {
+		return errors.New("")
+	}
+
+	return nil
+}
+
+func (m MessageRepositoryMock) GetMesssagesByConversation(conversationId string) []entity.Message {
+	id, err := primitive.ObjectIDFromHex(conversationId)
+
+	if err != nil {
+		return make([]entity.Message, 0)
+	}
+
+	return []entity.Message{
+		{
+			ID:             primitive.NewObjectID(),
+			CreatedAt:      time.Now(),
+			UpdatedAt:      time.Now(),
+			From:           1,
+			To:             2,
+			ConversationId: id,
+			Text:           "Text",
+		},
+	}
+}
